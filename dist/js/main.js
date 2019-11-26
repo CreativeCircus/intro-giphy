fb7433d7c14a2bd42ffee693bd727d7f14c4aea1
Chris Silich <chris.silich@gmail.com>
2019-11-26 15:35:58 -0500

    done!

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,7 @@
 var getGif = function getGif(searchTerm, callback) {
   // learn about how the giphy API wants you to construct your URLs to make a request here:
   // https://developers.giphy.com/docs/api/endpoint#search
-  var GIPHY_API = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&rating=G&'; // axios is a package for fetching data via ajax.
+  var GIPHY_API = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&rating=G&offset=' + Math.floor(Math.random() * 10) + '&'; // axios is a package for fetching data via ajax.
 
   axios.get(GIPHY_API + 'q=' + searchTerm).then(function (response) {
     callback(response.data.data);
@@ -20,9 +20,83 @@ var getGif = function getGif(searchTerm, callback) {
 /*
  * simple example of how to get cat gifs and console log the results
  */
+// getGif('cat', function(gifData) {
+// 	console.log(gifData)
+// })
 
 
-getGif('cat', function (gifData) {
-  console.log(gifData);
+var fade = 3;
+var giphyIndex = 0;
+var giphyData = null;
+var $field = document.querySelector('.search-field');
+var $background = document.querySelector('.background');
+var $foreground = document.querySelector('.foreground');
+setInterval(function () {
+  fade -= 0.025;
+  if (fade < 0.1) fade = 0.1;
+  $field.style.opacity = fade;
+}, 33);
+$field.addEventListener('mousemove', function () {
+  fade = 3;
 });
+$field.addEventListener('click', function () {
+  fade = 3;
+});
+$field.addEventListener('keyup', function () {
+  fade = 3;
+
+  if ($field.value.length > 0) {
+    doSearch();
+  } else {
+    $background.style.backgroundImage = "";
+    $foreground.style.backgroundImage = "";
+  }
+});
+
+var doSearch = function doSearch() {
+  getGif($field.value, handleGiphyResponse);
+};
+
+var handleGiphyResponse = function handleGiphyResponse(gifData) {
+  // console.log('got data', gifData)
+  giphyIndex = 0;
+  giphyData = gifData;
+  updateGifView();
+};
+
+var updateGifView = function updateGifView() {
+  // console.log('foo')
+  $background.style.backgroundImage = "url(".concat(giphyData[giphyIndex].images.original.url, ")");
+  $foreground.style.backgroundImage = "url(".concat(giphyData[giphyIndex].images.original.url, ")");
+};
+
+window.addEventListener('keyup', function (event) {
+  console.log(event.code);
+
+  if (event.code === "ArrowLeft") {
+    giphyIndex--;
+    if (giphyIndex < 0) giphyIndex = 0;
+    updateGifView();
+  } else if (event.code === "ArrowRight") {
+    giphyIndex++;
+    if (giphyIndex >= giphyData.length) giphyIndex = giphyData.length - 1;
+    updateGifView();
+  }
+});
+$field.focus();
+setTimeout(function () {
+  $field.value = 'c';
+  doSearch();
+  fade = 3;
+}, 2000);
+setTimeout(function () {
+  $field.value = 'ca';
+  doSearch();
+  fade = 3;
+}, 2500);
+setTimeout(function () {
+  $field.value = 'cat';
+  doSearch();
+  fade = 3;
+}, 3000);
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@
 let getGif = function(searchTerm, callback) {
 	// learn about how the giphy API wants you to construct your URLs to make a request here:
 	// https://developers.giphy.com/docs/api/endpoint#search
-	const GIPHY_API = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&rating=G&';
+	const GIPHY_API = 'https://api.giphy.com/v1/gifs/search?api_key=' + GIPHY_API_KEY + '&rating=G&offset=' + Math.floor(Math.random()*10) + '&';
 	
 	// axios is a package for fetching data via ajax.
 	axios.get(GIPHY_API + 'q=' + searchTerm)
@@ -24,6 +24,88 @@ let getGif = function(searchTerm, callback) {
 /*
  * simple example of how to get cat gifs and console log the results
  */
-getGif('cat', function(gifData) {
-	console.log(gifData)
+// getGif('cat', function(gifData) {
+// 	console.log(gifData)
+// })
+
+let fade = 3
+let giphyIndex = 0
+let giphyData = null
+
+let $field = document.querySelector('.search-field')
+let $background = document.querySelector('.background')
+let $foreground = document.querySelector('.foreground')
+
+setInterval(function() {
+	fade -= 0.025
+	if (fade < 0.1) fade = 0.1
+	$field.style.opacity = fade
+}, 33)
+
+$field.addEventListener('mousemove', function() {
+	fade = 3
 })
+
+$field.addEventListener('click', function() {
+	fade = 3
+})
+
+$field.addEventListener('keyup', function() {
+	fade = 3
+	if ($field.value.length > 0) {
+		doSearch()
+	} else {
+		$background.style.backgroundImage = ``
+		$foreground.style.backgroundImage = ``
+	}
+})
+
+let doSearch = function() {
+	getGif($field.value, handleGiphyResponse)
+}
+
+let handleGiphyResponse = function(gifData) {
+	// console.log('got data', gifData)
+	giphyIndex = 0
+	giphyData = gifData
+	updateGifView()
+}
+
+let updateGifView = function() {
+	// console.log('foo')
+	$background.style.backgroundImage = `url(${giphyData[giphyIndex].images.original.url})`
+	$foreground.style.backgroundImage = `url(${giphyData[giphyIndex].images.original.url})`
+}
+
+window.addEventListener('keyup', function(event) {
+	console.log( event.code )
+	if (event.code === "ArrowLeft") {
+		giphyIndex--
+		if (giphyIndex < 0) giphyIndex = 0
+		updateGifView()
+	} else if (event.code === "ArrowRight") {
+		giphyIndex++
+		if (giphyIndex >= giphyData.length) giphyIndex = giphyData.length-1
+		updateGifView()
+	} 
+})
+
+$field.focus()
+
+setTimeout(function() {
+	$field.value = 'c'
+	doSearch()
+	fade = 3
+}, 2000)
+
+setTimeout(function() {
+	$field.value = 'ca'
+	doSearch()
+	fade = 3
+}, 2500)
+
+setTimeout(function() {
+	$field.value = 'cat'
+	doSearch()
+	fade = 3
+}, 3000)
